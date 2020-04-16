17c2288bc0527f152ce2e550aeafad29bbb95222
dakhinray <dakhinray@gmail.com>
2020-04-16 13:08:19 +0100

    done with 4 tests

--- test/test_seqlen.py
+++ test/test_seqlen.py
@@ -18,22 +18,24 @@ def test_parse_args():
         setup = get_setup_file()
         assert setup == "/home/fenton/project/setup.py"
 
-import logging
+#import logging
 
 @contextmanager
 def capture(command, *args, **kwargs):
   out, sys.stdout = sys.stdout, StringIO()
+  err, sys.stderr = sys.stderr, StringIO()
   try:
     command(*args, **kwargs)
     sys.stdout.seek(0)
-    yield sys.stdout.read()
+    yield sys.stdout.read() + sys.stderr.read()
   finally:
     sys.stdout = out
+    sys.stderr = err
 
 
 class MyTestCase(unittest.TestCase):
     def test_case1(self):
-        log = logging.getLogger('Temp')
+        #log = logging.getLogger('Temp')
         path = './data'
         filePath = path + '/temp'
 
@@ -47,7 +49,7 @@ class MyTestCase(unittest.TestCase):
             f.write('{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853965, "read_id": "314309c0-09de-4291-a5e4-0ea288db74f3", "seqlen": 1459, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 10.377, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}'+
                     '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853965, "read_id": "db367858-24f8-46ec-b352-08cdf5388ad4", "seqlen": 1177, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 12.995, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}'+
                     '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853966, "read_id": "723b1945-74c3-40b3-a76a-ff6d6b3f8203", "seqlen": 1084, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 12.968, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}')
-        testargs = ["prog", '-q', path]
+        testargs = ["metrichor_challenge.py", '-q', path]
         with patch.object(sys, 'argv', testargs):
             with capture(metrichor_challenge.main) as output:
                 #log.warning( "jay ******************** : {}".format(output))
@@ -58,7 +60,7 @@ class MyTestCase(unittest.TestCase):
             shutil.rmtree(path)
 
     def test_case2(self):
-        log = logging.getLogger('Temp')
+        #log = logging.getLogger('Temp')
         path = './data'
         filePath = path + '/temp.data.json'
 
@@ -73,7 +75,59 @@ class MyTestCase(unittest.TestCase):
                 '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853965, "read_id": "314309c0-09de-4291-a5e4-0ea288db74f3", "seqlen": 1, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 10.377, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}\n' +
                 '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853965, "read_id": "db367858-24f8-46ec-b352-08cdf5388ad4", "seqlen": 2, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 12.995, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}\n' +
                 '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853966, "read_id": "723b1945-74c3-40b3-a76a-ff6d6b3f8203", "seqlen": 3, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 12.968, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}\n')
-        testargs = ["prog", '-q', path]
+        testargs = ["metrichor_challenge.py", '-q', path]
+        with patch.object(sys, 'argv', testargs):
+            with capture(metrichor_challenge.main) as output:
+                #log.warning("jay ******************** : {}".format(output))
+                self.assertEqual(str(output).strip(),'6')
+        if os.path.exists(filePath):
+            os.remove(filePath)
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+    def test_case3(self):
+        #log = logging.getLogger('Temp')
+        path = './data'
+        filePath = path + '/temp.data.json'
+
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        else:
+            self.assertTrue(False, 'Should not be here, folder should not exist!')
+        with open(filePath, 'w') as f:
+            f.write(
+                '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853965, "read_id": "314309c0-09de-4291-a5e4-0ea288db74f3", "seqlen": 1, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 10.377, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}\n' +
+                '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853965, "read_id": "db367858-24f8-46ec-b352-08cdf5388ad4", "seqlen": 2, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 12.995, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}\n' +
+                '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853966, "read_id": "723b1945-74c3-40b3-a76a-ff6d6b3f8203", "seqlen": 3, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 12.968, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}\n')
+        testargs = ["metrichor_challenge.py", path]
+        with patch.object(sys, 'argv', testargs):
+            with capture(metrichor_challenge.main) as output:
+                #log.warning("jay ******************** : {}".format(output))
+                self.assertEqual(str(output).strip(),'6')
+        if os.path.exists(filePath):
+            os.remove(filePath)
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+    def test_case4(self):
+        #log = logging.getLogger('Temp')
+        path = './data'
+        filePath = path + '/temp.data.json'
+
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        else:
+            self.assertTrue(False, 'Should not be here, folder should not exist!')
+        with open(filePath, 'w') as f:
+            f.write(
+                '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853965, "read_id": "314309c0-09de-4291-a5e4-0ea288db74f3", "seqlen": 1, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 10.377, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}\n' +
+                '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853965, "read_id": "db367858-24f8-46ec-b352-08cdf5388ad4", "seqlen": 2, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 12.995, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}\n' +
+                '{"format_conversion": {"alphabet_conversion": false, "header_corrected": false}, "barcode": "NA", "retcode": "PASS", "exit_status": "Workflow successful", "calibration": false, "barcode_detection": {"status": "1", "barcode": "NA", "barcode_score": 0.0}, "start_time": 1501853966, "read_id": "723b1945-74c3-40b3-a76a-ff6d6b3f8203", "seqlen": 3, "filename": "split_aa.fastq", "runid": "f9b53105df0f6e165aa09f824bd26cbcb4dfa93a", "mean_qscore": 12.968, "software": {"time_stamp": "2019-Aug-22 09:38:14", "version": "3.10.0", "component": "homogeny"}}\n')
+        testargs = ["metrichor_challenge.py"]
         with patch.object(sys, 'argv', testargs):
             with capture(metrichor_challenge.main) as output:
                 #log.warning("jay ******************** : {}".format(output))
@@ -84,7 +138,8 @@ class MyTestCase(unittest.TestCase):
             shutil.rmtree(path)
 
 
+
 if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stderr)
-    logging.getLogger("SomeTest.testSomething").setLevel(logging.DEBUG)
+    #logging.basicConfig(stream=sys.stderr)
+    #logging.getLogger("SomeTest.testSomething").setLevel(logging.DEBUG)
     unittest.main()
